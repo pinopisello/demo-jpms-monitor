eb62b602d4a1bb0d737ff14cdce8bbc8910882d4
glocon <glocon@nike>
2018-06-27 11:47:14 -0700

    update 27/6/18

---
+++ monitor.observer/src/main/java/monitor/observer/DisconnectedServiceObserver.java
@@ -0,0 +1,15 @@
+package monitor.observer;
+
+public class DisconnectedServiceObserver implements ServiceObserver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public DiagnosticDataPoint gatherDataFromService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
